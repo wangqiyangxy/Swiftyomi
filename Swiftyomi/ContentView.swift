207f252c8f92e1e519eb74e9c6c3ba185d585d5e
wangqiyangxy <wangqiyangxy@outlook.com>
2024-02-12 11:31:44 +0800

    Initial Commit

---
+++ Swiftyomi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiftyomi
+//
+//  Created by renyi on 2024/2/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiftyomi/SwiftyomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyomiApp.swift
+//  Swiftyomi
+//
+//  Created by renyi on 2024/2/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
